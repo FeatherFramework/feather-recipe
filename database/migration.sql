b4d8b04ffe22efbdaa84fce6c8ad305497e6aed4
DavFount <47730893+DavFount@users.noreply.github.com>
2023-08-12 00:53:24 -0700

    Temporarily disabled seed.sql, Added role foreign key, updated constraints

--- database/migration.sql
+++ database/migration.sql
@@ -6,13 +6,18 @@ CREATE TABLE `roles` (
     UNIQUE INDEX role_name (`name`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
+INSERT INTO roles (name, level) VALUES ('general', 0);
+INSERT INTO roles (name, level) VALUES ('admin', 99);
+
 CREATE TABLE `users` (
     `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
     `username` varchar(255) NOT NULL,
     `license` varchar(255) NOT NULL,
+    `role_id` bigint NULL DEFAULT 1,
     `created_at` TimeStamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` TimeStamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
+    `updated_at` TimeStamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `FK_Role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `characters` (
@@ -30,7 +35,7 @@ CREATE TABLE `characters` (
     `z` decimal(15,10) NOT NULL,
     `lang` varchar(255) NOT NULL,
     `created_at` TimeStamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` TimeStamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+    `updated_at` TimeStamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     CONSTRAINT `FK_User` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT `FK_Role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON UPDATE CASCADE
